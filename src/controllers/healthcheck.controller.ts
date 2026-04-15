556997d8d7e39efa4034aaba893cf38ad15db118
gopaladhikari <gopuadks@gmail.com>
2026-04-15 16:12:49 +0545

    healthcheck route wit async handler

--- src/controllers/healthcheck.controller.ts
+++ src/controllers/healthcheck.controller.ts
@@ -1,20 +1,13 @@
-import { ApiError, ApiResponse } from "../utils/api-response.js";
+import { ApiError, ApiResponse } from "../utils/api-responses.js";
 import type { Response, Request } from "express";
+import { asyncHandler } from "../utils/async-handler.js";
 
-export const healthCheck = (_req: Request, res: Response) => {
-  try {
-    res
-      .status(200)
-      .json(new ApiResponse(200, "Health Check Successful", {}));
-  } catch (error) {
-    res
-      .status(500)
-      .json(
-        new ApiError(
-          500,
-          "Health Check Failed",
-          (error as Error).message,
-        ),
-      );
-  }
-};
+export const healthCheck = asyncHandler(
+  async (_req: Request, res: Response) => {
+    try {
+      res.status(200).json(new ApiResponse(200, "OK", true));
+    } catch (error) {
+      throw new ApiError(500, (error as Error).message);
+    }
+  },
+);
