ed0809815dc7426a05f4af0f1ab7429bba459a49
gopaladhikari <gopuadks@gmail.com>
2026-04-15 11:59:20 +0545

    express configuration

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env["CORS_ORIGIN"] || "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+export { app };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import express from "express";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "../.env",
 });
-
-const app = express();
 
 const PORT = process.env["PORT"] || 3000;
 
