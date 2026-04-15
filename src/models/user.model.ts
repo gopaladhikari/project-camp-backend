4e18ff5e741adf6a377cb7f6c04ad012cd3b5511
gopaladhikari <gopuadks@gmail.com>
2026-04-15 16:47:16 +0545

    user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -65,5 +66,10 @@ const userSchema = new Schema(
     timestamps: true,
   },
 );
+
+userSchema.pre("save", async function () {
+  if (this.password && this.isModified("password"))
+    this.password = await bcrypt.hash(this.password, 10);
+});
 
 export const User = mongoose.model("User", userSchema);
