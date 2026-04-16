18156445a5a6bad796ed39de52fcc251e8399436
gopaladhikari <gopuadks@gmail.com>
2026-04-16 13:27:06 +0545

    email temlate and nodemailer configuration

---
+++ src/utils/mail.ts
@@ -0,0 +1,90 @@
+import Mailgen, { type Content } from "mailgen";
+import nodemailer from "nodemailer";
+
+const host = process.env.MAILTRAP_HOST as string;
+const port = process.env.MAILTRAP_PORT as string;
+const user = process.env.MAILTRAP_USER as string;
+const pass = process.env.MAILTRAP_PASS as string;
+
+export const emailVerificationEmailTemplate = (
+  emailVerificationUrl: string,
+  username: string,
+): Content => {
+  return {
+    body: {
+      name: username,
+      intro:
+        "Welcome to Project Camp!, We are excited to have you on board!",
+      action: {
+        instructions: "Click the button below to verify your email",
+        button: {
+          link: emailVerificationUrl,
+          text: "Verify Email",
+          color: "#22BC66",
+        },
+      },
+      outro: "If you did not request this email, please ignore it.",
+    },
+  };
+};
+
+export const forgotPasswordEmailTemplate = (
+  forgotPasswordUrl: string,
+  username: string,
+): Content => {
+  return {
+    body: {
+      name: username,
+      intro:
+        "You have received this email because a password reset request for your account was received.",
+      action: {
+        instructions:
+          "Click the button below to reset your password:",
+        button: {
+          text: "Reset your password",
+          link: forgotPasswordUrl,
+          color: "#22BC66",
+        },
+      },
+      outro: "If you did not request this email, please ignore it.",
+    },
+  };
+};
+
+export const sendEmail = async (
+  options: Content,
+  email: string,
+  subject: string,
+) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Project Camp",
+      link: "https://projectcamp.com",
+    },
+  });
+
+  const emailtextual = mailGenerator.generatePlaintext(options);
+  const emailHTML = mailGenerator.generate(options);
+
+  const transport = nodemailer.createTransport({
+    host,
+    port: parseInt(port),
+    auth: {
+      user,
+      pass,
+    },
+  });
+
+  try {
+    await transport.sendMail({
+      from: "Camp Project <projectcamp@projectcamp.com>",
+      to: email,
+      subject: subject,
+      text: emailtextual,
+      html: emailHTML,
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
