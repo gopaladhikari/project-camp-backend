966e3125e0b31f3958d8e318c1f340a8cf0087c4
gopaladhikari <gopuadks@gmail.com>
2026-04-18 23:04:12 +0545

    task routes

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -7,9 +7,31 @@ import {
   deleteTaskById,
 } from "../controllers/task.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
+import { rbacMiddleware } from "../middlewares/rbac.middleware.js";
+import { UserRoles } from "../utils/constants.js";
 
 const taskRouter = Router();
 
 taskRouter.use(verifyJwt);
+
+taskRouter
+  .route("/:projectId")
+  .get(getTasks)
+  .post(
+    rbacMiddleware([UserRoles.ADMIN, UserRoles.PROJECT_ADMIN]),
+    createTask,
+  );
+
+taskRouter
+  .route("/:projectId/t/:taskId")
+  .get(getTaskById)
+  .put(
+    rbacMiddleware([UserRoles.ADMIN, UserRoles.PROJECT_ADMIN]),
+    updateTaskById,
+  )
+  .delete(
+    rbacMiddleware([UserRoles.ADMIN, UserRoles.PROJECT_ADMIN]),
+    deleteTaskById,
+  );
 
 export { taskRouter };
